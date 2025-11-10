71f20bd981096912f12798fe05c70a72ebf1339c
Prince Chauhan <princechauhan5422@gmail.com>
2025-11-10 12:20:01 +0530

    Rebuild web fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "905fdbff53644f10c482287dd80ef381
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "00f8d95c7a4487d30d9b3b8e7d8ffa4a",
+"flutter_bootstrap.js": "05cf95123a9c1b2627551f4c1d8eba2d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
